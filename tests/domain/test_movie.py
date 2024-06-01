739caca18939bcb3fbee74b3df48f348ae910e76
aleroux <alex.leroux89@gmail.com>
2024-06-01 11:45:29 +0200

    Movie unit tests

---
+++ tests/domain/test_movie.py
@@ -0,0 +1,17 @@
+from src.domain.entities.movie import Movie
+
+
+def test_movie_title():
+    """
+    Test case for the `title` property of the Movie class.
+    """
+    movie = Movie("Sunrise: A Song of Two Humans", 1927)
+    assert movie.title == "Sunrise: A Song of Two Humans"
+
+
+def test_movie_release_year():
+    """
+    Test case for the `release_year` property of the Movie class.
+    """
+    movie = Movie("Singin' in the Rain", 1952)
+    assert movie.release_year == 1952
