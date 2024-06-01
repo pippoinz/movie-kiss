11614b207224ddecf79f2251b8a4a2a0c6f071e6
aleroux <alex.leroux89@gmail.com>
2024-06-01 12:11:36 +0200

    pylint fixes

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
+"""
+This module contains the setup script for the movie-kiss project.
+
+The movie-kiss project is a Python project based on clean architecture for movie selection.
+
+Author: Alexandre Leroux
+Email: alex.leroux89@gmail.com
+"""
+
 from setuptools import setup, find_packages
 
 setup(
-    name='movie-kiss',
-    version='0.1.0',
-    description='A movie recommendation system',
-    author='Alexandre Leroux',
-    author_email='alex.leroux89@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    name="movie-kiss",
+    version="0.1.0",
+    description="A Python project based on clean architecture for movie selection",
+    author="Alexandre Leroux",
+    author_email="alex.leroux89@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
     ],
 )
