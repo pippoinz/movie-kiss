3050cac6154e5fe691428c9ceb8e7e5a9507530b
aleroux <alex.leroux89@gmail.com>
2024-06-04 22:22:38 +0200

    remove unnecessary imports

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,7 +1,4 @@
-"""
-This file is the entry point for the domain layer.
-"""
-
-# Import domain modules
-from .entities import *
-from .interfaces import *
+# This file is intentionally left blank.
+# Add your entity modules in this directory to represent specific entities in the domain.
+# Each entity module should define the structure and behavior of a specific entity.
+# Entities typically encapsulate the business logic and state of the application.
